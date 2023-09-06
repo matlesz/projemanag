6f56bb37cb46bd5f658e436489af8bf3fc00297b
Mat <matlesz@gmail.com>
2023-09-06 16:38:53 +0100

    Preparing XML for the Drawer Navigation

--- app/src/main/java/com/projemanag/activities/MainActivity.kt
+++ app/src/main/java/com/projemanag/activities/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.projemanag.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.projemanag.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     /**
      * This function is auto created by Android when the Activity Class is created.
