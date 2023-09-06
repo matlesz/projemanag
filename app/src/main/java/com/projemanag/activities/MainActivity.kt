da828f421f713023acb82fe129266473663c354f
Mat <matlesz@gmail.com>
2023-09-06 16:39:57 +0100

    Adding drawer Functionality

--- app/src/main/java/com/projemanag/activities/MainActivity.kt
+++ app/src/main/java/com/projemanag/activities/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.projemanag.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.projemanag.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : BaseActivity() {
+// TODO (Step 6: Implement the NavigationView.OnNavigationItemSelectedListener and add the implement members of it.)
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -16,5 +23,90 @@ class MainActivity : BaseActivity() {
 
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
+
+        // TODO (Step 4: Call the setup action bar function here.)
+        // START
+        setupActionBar()
+        // END
+
+        // TODO (Step 8: Assign the NavigationView.OnNavigationItemSelectedListener to navigation view.)
+        // START
+        // Assign the NavigationView.OnNavigationItemSelectedListener to navigation view.
+        nav_view.setNavigationItemSelectedListener(this)
+        // END
     }
+
+    // TODO (Step 5: Add a onBackPressed function and check if the navigation drawer is open or closed.)
+    // START
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            // A double back press function is added in Base Activity.
+            doubleBackToExit()
+        }
+    }
+    // END
+
+    // TODO (Step 7: Implement members of NavigationView.OnNavigationItemSelectedListener.)
+    // START
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        // TODO (Step 9: Add the click events of navigation menu items.)
+        // START
+        when (menuItem.itemId) {
+            R.id.nav_my_profile -> {
+
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_sign_out -> {
+                // Here sign outs the user from firebase in this device.
+                FirebaseAuth.getInstance().signOut()
+
+                // Send the user to the intro screen of the application.
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        // END
+        return true
+    }
+    // END
+
+    // TODO (Step 1: Create a function to setup action bar.)
+    // START
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        // TODO (Step 3: Add click event for navigation in the action bar and call the toggleDrawer function.)
+        // START
+        toolbar_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+        // END
+    }
+    // END
+
+    // TODO (Step 2: Create a function for opening and closing the Navigation Drawer.)
+    // START
+    /**
+     * A function for opening and closing the Navigation Drawer.
+     */
+    private fun toggleDrawer() {
+
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+    // END
 }
