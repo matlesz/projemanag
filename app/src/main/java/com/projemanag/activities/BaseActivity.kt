bc2bf6ff10afa3e2b5560ca54569558e2a71d46b
Mat <matlesz@gmail.com>
2023-09-06 17:18:42 +0100

    Preparing the Cards Color Dialogs and Adapter

--- app/src/main/java/com/projemanag/activities/BaseActivity.kt
+++ app/src/main/java/com/projemanag/activities/BaseActivity.kt
@@ -59,9 +59,9 @@ open class BaseActivity : AppCompatActivity() {
 
         this.doubleBackToExitPressedOnce = true
         Toast.makeText(
-            this,
-            resources.getString(R.string.please_click_back_again_to_exit),
-            Toast.LENGTH_SHORT
+                this,
+                resources.getString(R.string.please_click_back_again_to_exit),
+                Toast.LENGTH_SHORT
         ).show()
 
         Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
@@ -69,13 +69,13 @@ open class BaseActivity : AppCompatActivity() {
 
     fun showErrorSnackBar(message: String) {
         val snackBar =
-            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
         val snackBarView = snackBar.view
         snackBarView.setBackgroundColor(
-            ContextCompat.getColor(
-                this@BaseActivity,
-                R.color.snackbar_error_color
-            )
+                ContextCompat.getColor(
+                        this@BaseActivity,
+                        R.color.snackbar_error_color
+                )
         )
         snackBar.show()
     }
---
+++ app/src/main/java/com/projemanag/activities/CardDetailsActivity.kt
@@ -0,0 +1,249 @@
+package com.projemanag.activities
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.projemanag.R
+import com.projemanag.dialogs.LabelColorListDialog
+import com.projemanag.firebase.FirestoreClass
+import com.projemanag.model.Board
+import com.projemanag.model.Card
+import com.projemanag.model.Task
+import com.projemanag.utils.Constants
+import kotlinx.android.synthetic.main.activity_card_details.*
+
+class CardDetailsActivity : BaseActivity() {
+
+    // A global variable for board details
+    private lateinit var mBoardDetails: Board
+    // A global variable for task item position
+    private var mTaskListPosition: Int = -1
+    // A global variable for card item position
+    private var mCardPosition: Int = -1
+    // TODO (Step 9: Add a global variable for selected label color)
+    // START
+    // A global variable for selected label color
+    private var mSelectedColor: String = ""
+    // END
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_details)
+
+        getIntentData()
+
+        setupActionBar()
+
+        et_name_card_details.setText(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
+        et_name_card_details.setSelection(et_name_card_details.text.toString().length) // The cursor after the string length
+
+        // TODO (Step 11: Add a click event for selecting a label color and launch the dialog.)
+        // START
+        tv_select_label_color.setOnClickListener {
+            labelColorsListDialog()
+        }
+        // END
+
+        btn_update_card_details.setOnClickListener {
+            if (et_name_card_details.text.toString().isNotEmpty()) {
+                updateCardDetails()
+            } else {
+                Toast.makeText(this@CardDetailsActivity, "Enter card name.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu to use in the action bar
+        menuInflater.inflate(R.menu.menu_delete_card, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_delete_card -> {
+                alertDialogForDeleteCard(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_card_details_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+
+        toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    // A function to get all the data that is sent through intent.
+    private fun getIntentData() {
+
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL) as Board
+        }
+    }
+
+    /**
+     * A function to get the result of add or updating the task list.
+     */
+    fun addUpdateTaskListSuccess() {
+
+        hideProgressDialog()
+
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
+    /**
+     * A function to update card details.
+     */
+    private fun updateCardDetails() {
+
+        // TODO (Step 13: Pass the selected label color of the card in the data model class.)
+        // START
+        // Here we have updated the card name using the data model class.
+        val card = Card(
+            et_name_card_details.text.toString(),
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo,
+            mSelectedColor
+        )
+        // END
+
+        // Here we have assigned the update card details to the task list using the card position.
+        mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
+
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, mBoardDetails)
+    }
+
+    /**
+     * A function to show an alert dialog for the confirmation to delete the card.
+     */
+    private fun alertDialogForDeleteCard(cardName: String) {
+        val builder = AlertDialog.Builder(this)
+        //set title for alert dialog
+        builder.setTitle(resources.getString(R.string.alert))
+        //set message for alert dialog
+        builder.setMessage(
+            resources.getString(
+                R.string.confirmation_message_to_delete_card,
+                cardName
+            )
+        )
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        //performing positive action
+        builder.setPositiveButton(resources.getString(R.string.yes)) { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+            deleteCard()
+        }
+        //performing negative action
+        builder.setNegativeButton(resources.getString(R.string.no)) { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
+    }
+
+    /**
+     * A function to delete the card from the task list.
+     */
+    private fun deleteCard() {
+
+        // Here we have got the cards list from the task item list using the task list position.
+        val cardsList: ArrayList<Card> = mBoardDetails.taskList[mTaskListPosition].cards
+        // Here we will remove the item from cards list using the card position.
+        cardsList.removeAt(mCardPosition)
+
+        val taskList: ArrayList<Task> = mBoardDetails.taskList
+        taskList.removeAt(taskList.size - 1)
+
+        taskList[mTaskListPosition].cards = cardsList
+
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, mBoardDetails)
+    }
+
+    // TODO (Step 7: Create a function to remove the text and set the label color to the TextView.)
+    // START
+    /**
+     * A function to remove the text and set the label color to the TextView.
+     */
+    private fun setColor() {
+        tv_select_label_color.text = ""
+        tv_select_label_color.setBackgroundColor(Color.parseColor(mSelectedColor))
+    }
+    // END
+
+    // TODO (Step 6: Create a function to add some static label colors in the list.)
+    // START
+    /**
+     * A function to add some static label colors in the list.
+     */
+    private fun colorsList(): ArrayList<String> {
+
+        val colorsList: ArrayList<String> = ArrayList()
+        colorsList.add("#43C86F")
+        colorsList.add("#0C90F1")
+        colorsList.add("#F72400")
+        colorsList.add("#7A8089")
+        colorsList.add("#D57C1D")
+        colorsList.add("#770000")
+        colorsList.add("#0022F8")
+
+        return colorsList
+    }
+    // END
+
+    // TODO (Step 10: Create a function to launch the label color list dialog.)
+    // START
+    /**
+     * A function to launch the label color list dialog.
+     */
+    private fun labelColorsListDialog() {
+
+        val colorsList: ArrayList<String> = colorsList()
+
+        val listDialog = object : LabelColorListDialog(
+            this@CardDetailsActivity,
+            colorsList,
+            resources.getString(R.string.str_select_label_color)
+        ) {
+            override fun onItemSelected(color: String) {
+                mSelectedColor = color
+                setColor()
+            }
+        }
+        listDialog.show()
+    }
+    // END
+}
