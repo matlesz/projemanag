396ee2fef0aa78edc82523ea73f939a3c29a3aa3
Mat <matlesz@gmail.com>
2023-09-06 16:05:15 +0100

    Setting up The Intro Screen

--- app/src/main/java/com/projemanag/SplashActivity.kt
+++ app/src/main/java/com/projemanag/SplashActivity.kt
@@ -2,14 +2,12 @@ package com.projemanag
 
 import android.content.Intent
 import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
-// TODO (Step 1: Add new activity named as splash activity.)
-// START
 class SplashActivity : AppCompatActivity() {
 
     /**
@@ -21,24 +19,23 @@ class SplashActivity : AppCompatActivity() {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_splash)
 
-        // TODO (Step 6: Add the full screen flags here.)
         // This is used to hide the status bar and make the splash screen as a full screen activity.
-        // START
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        // END
 
-        // TODO (Step 7: Add the file in the custom font file to the assets folder. And add the below line of code to apply it to the title TextView.)
-        // Steps for adding the assets folder are :
-        // Right click on the "app" package and GO TO ==> New ==> Folder ==> Assets Folder ==> Finish.
-        // START
         // This is used to get the file from the assets folder and set it to the title textView.
         val typeface: Typeface =
             Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = typeface
-        // END
+
+        // TODO (Step 9: Here we will launch the Intro Screen after the splash screen using the handler. As using handler the splash screen will disappear after what we give to the handler.)
+        // Adding the handler to after the a task after some delay.
+        Handler().postDelayed({
+            // Start the Intro Activity
+            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            finish() // Call this when your activity is done and should be closed.
+        }, 2500) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
     }
 }
-// END
