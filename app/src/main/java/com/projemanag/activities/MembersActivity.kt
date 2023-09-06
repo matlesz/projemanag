92426d5c9e9e4c4ff3d0c43faa556360276a170e
Mat <matlesz@gmail.com>
2023-09-06 17:05:55 +0100

    Preparing the Members Activity

---
+++ app/src/main/java/com/projemanag/activities/MembersActivity.kt
@@ -0,0 +1,15 @@
+package com.projemanag.activities
+
+import android.os.Bundle
+import com.projemanag.R
+
+// TODO (Step 1: Create a MembersActivity.)
+// START
+class MembersActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_members)
+    }
+}
+// END
